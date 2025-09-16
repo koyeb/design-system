28748d9d34302e24e740c950a68f39c6ca7a7ec0
nils <nilscox.dev@gmail.com>
2025-09-16 12:40:01 +0200

    improve combobox api

--- src/combobox/combobox.tsx
+++ src/combobox/combobox.tsx
@@ -1,6 +1,10 @@
 import {
+  ElementRects,
+  Elements,
   FloatingPortal,
+  Placement,
   UseFloatingOptions,
+  UseFloatingReturn,
   UseTransitionStylesProps,
   autoUpdate,
   flip,
@@ -12,28 +16,27 @@ import {
 import clsx from 'clsx';
 import * as downshift from 'downshift';
 import { ChevronsUpDownIcon } from 'lucide-react';
-import { CSSProperties, createContext, useContext } from 'react';
+import { CSSProperties, createContext, use } from 'react';
 
 import { Field } from '../field/field';
 import { InputBox } from '../input/input';
 
-type DownshiftCombobox<T> = ReturnType<typeof downshift.useCombobox<T>>;
-type Floating = ReturnType<typeof useFloating>;
 type FloatingTransition = { isMounted: boolean; styles: CSSProperties };
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type ComboboxContext = DownshiftCombobox<any> & {
-  floating: Floating;
+type ComboboxContext<T = unknown> = downshift.UseComboboxReturnValue<T> & {
+  floating: UseFloatingReturn;
   transition: FloatingTransition;
 };
 
-const comboboxContext = createContext<ComboboxContext | null>(null);
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const comboboxContext = createContext<ComboboxContext<any> | null>(null);
 
 export const Combobox = {
   stateChangeTypes: downshift.useCombobox.stateChangeTypes,
   context: comboboxContext,
   Provider: comboboxContext.Provider,
   useCombobox,
+  useComboboxContext,
   Root,
   Input,
   Dropdown,
@@ -42,27 +45,34 @@ export const Combobox = {
   DropdownMenu,
 };
 
-function useCombobox<T>(
-  comboboxProps: downshift.UseComboboxProps<T>,
-  floatingProps?: UseFloatingOptions,
-  transitionProps?: UseTransitionStylesProps,
-): ComboboxContext {
-  const combobox = downshift.useCombobox(comboboxProps);
+type UseComboboxProps<T> = {
+  items?: T[];
+  combobox: Omit<downshift.UseComboboxProps<T>, 'items'>;
+  floating?: UseFloatingOptions;
+  transition?: UseTransitionStylesProps;
+  placement?: Placement;
+  offset?: number;
+  flip?: boolean;
+  matchReferenceSize?: boolean;
+};
+
+function useCombobox<T>({
+  items,
+  combobox: comboboxProps,
+  floating: floatingProps,
+  transition: transitionProps,
+  ...props
+}: UseComboboxProps<T>): ComboboxContext<T> {
+  const combobox = downshift.useCombobox({ items: items ?? [], ...comboboxProps });
 
   const floating = useFloating({
     whileElementsMounted: autoUpdate,
     open: combobox.isOpen,
-    placement: 'bottom-start',
+    placement: props.placement,
     middleware: [
-      offset(4),
-      flip(),
-      size({
-        apply({ rects, elements }) {
-          Object.assign(elements.floating.style, {
-            width: `${rects.reference.width}px`,
-          });
-        },
-      }),
+      props.offset !== undefined && offset(props.offset),
+      props.flip && flip(),
+      props.matchReferenceSize && size({ apply: applyMatchReferenceSize }),
     ],
     ...floatingProps,
   });
@@ -79,26 +89,30 @@ function useCombobox<T>(
   };
 }
 
-type ComboboxProps<T> = {
-  items: T[];
-  combobox?: Omit<downshift.UseComboboxProps<T>, 'items'>;
-  floating?: UseFloatingOptions;
-  transition?: UseTransitionStylesProps;
+function applyMatchReferenceSize({ rects, elements }: { rects: ElementRects; elements: Elements }) {
+  Object.assign(elements.floating.style, {
+    width: `${rects.reference.width}px`,
+  });
+}
+
+function useComboboxContext() {
+  const context = use(comboboxContext);
+
+  if (context === null) {
+    throw new Error(`Missing combobox provider`);
+  }
+
+  return context;
+}
+
+type ComboboxProps<T> = UseComboboxProps<T> & {
   input?: React.ReactNode;
   root?: HTMLElement;
   children: React.ReactNode;
 };
 
-function Root<T>({
-  items,
-  combobox,
-  floating,
-  transition,
-  input = <Combobox.Input />,
-  root,
-  children,
-}: ComboboxProps<T>) {
-  const value = Combobox.useCombobox({ items, ...combobox }, floating, transition);
+function Root<T>({ input = <Combobox.Input />, root, children, ...props }: ComboboxProps<T>) {
+  const value = Combobox.useCombobox(props);
 
   return (
     <Combobox.Provider value={value}>
@@ -109,14 +123,14 @@ function Root<T>({
 }
 
 function Input({ start, ...props }: React.ComponentProps<typeof InputBox>) {
-  const combobox = useContext(comboboxContext);
+  const combobox = useComboboxContext();
 
   return (
     <InputBox
-      {...combobox?.getInputProps(props)}
+      {...combobox.getInputProps(props)}
       start={start}
       end={
-        <button {...combobox?.getToggleButtonProps()} className="px-2">
+        <button {...combobox.getToggleButtonProps()} className="px-2">
           <ChevronsUpDownIcon className="size-4" />
         </button>
       }
@@ -125,15 +139,15 @@ function Input({ start, ...props }: React.ComponentProps<typeof InputBox>) {
 }
 
 function Dropdown({ className, ...props }: React.ComponentProps<'div'>) {
-  const combobox = useContext(comboboxContext);
+  const { floating, transition } = useComboboxContext();
 
   return (
     <div
-      ref={combobox?.floating.refs.setFloating}
-      style={{ ...combobox?.floating.floatingStyles, ...combobox?.transition.styles }}
+      ref={floating.refs.setFloating}
+      style={{ ...floating.floatingStyles, ...transition.styles }}
       className={clsx(
         'z-50 rounded-md border bg-neutral shadow-md',
-        { hidden: !combobox?.transition.isMounted },
+        { hidden: !transition.isMounted },
         className,
       )}
       {...props}
@@ -142,17 +156,17 @@ function Dropdown({ className, ...props }: React.ComponentProps<'div'>) {
 }
 
 function Menu({ className, ...props }: React.ComponentProps<'ul'>) {
-  const combobox = useContext(comboboxContext);
+  const combobox = useComboboxContext();
 
-  return <ul {...combobox?.getMenuProps(props)} className={clsx('p-1', className)} />;
+  return <ul {...combobox.getMenuProps(props)} className={clsx('p-1', className)} />;
 }
 
 function MenuItem({ className, ...props }: { item: unknown } & React.ComponentProps<'li'>) {
-  const combobox = useContext(comboboxContext);
+  const combobox = useComboboxContext();
 
   return (
     <li
-      {...combobox?.getItemProps(props)}
+      {...combobox.getItemProps(props)}
       className={clsx(
         'row cursor-pointer items-center rounded-md px-2 py-1 aria-disabled:cursor-default aria-selected:bg-muted',
         className,
