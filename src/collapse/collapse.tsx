111091dcf9c6d449a80eae30e0504204d27bd24c
nils <nilscox.dev@gmail.com>
2025-10-03 12:17:34 +0200

    allow to specify a className in the Collapse component

--- src/collapse/collapse.tsx
+++ src/collapse/collapse.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import { AnimatePresence, LazyMotion, domAnimation, m } from 'framer-motion';
 
 const hidden = { opacity: 0.5, height: 0 };
@@ -6,10 +7,11 @@ const visible = { opacity: 1, height: 'auto' };
 type CollapseProps = {
   open: boolean;
   keepMounted?: boolean;
+  className?: string;
   children: React.ReactNode;
 };
 
-export function Collapse({ open, keepMounted, children }: CollapseProps) {
+export function Collapse({ open, keepMounted, className, children }: CollapseProps) {
   return (
     <LazyMotion features={domAnimation}>
       <AnimatePresence initial={false}>
@@ -19,7 +21,7 @@ export function Collapse({ open, keepMounted, children }: CollapseProps) {
             animate={open ? visible : hidden}
             exit={hidden}
             transition={{ duration: 0.2, ease: 'easeOut' }}
-            className="overflow-hidden"
+            className={clsx('overflow-hidden', className)}
           >
             {children}
           </m.div>
